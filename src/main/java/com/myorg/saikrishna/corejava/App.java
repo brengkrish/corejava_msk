695bd5dd3ba4ff200affd7c4a6c6862411cbc1ba
krish <brengkrish@gmail.com>
2016-05-06 00:24:23 -0400

    adding files

---
+++ src/main/java/com/myorg/saikrishna/corejava/App.java
@@ -0,0 +1,13 @@
+package com.myorg.saikrishna.corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/myorg/saikrishna/corejava/InputfromCline.java
@@ -0,0 +1,15 @@
+package com.myorg.saikrishna.corejava;
+
+public class InputfromCline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = Integer.parseInt(args[0]);
+		int j = Integer.parseInt(args[1]);
+		
+		int sum = i+j;
+		System.out.println("sum = " +sum);
+
+	}
+
+}
